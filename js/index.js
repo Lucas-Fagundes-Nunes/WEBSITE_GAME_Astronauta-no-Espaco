f952e5a0430967013ecd2a64c6270980f0ecfdc1
Lucas Fagundes Nunes <113182658+Lucas-Fagundes-Nunes@users.noreply.github.com>
2023-10-05 11:40:00 -0300

    Adicionando mecânica do jogo
    Mecânicas como movimentação personagem, criação de plataforma

---
+++ js/index.js
@@ -0,0 +1,167 @@
+const canvas =document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const gravity = 1.5;
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100,
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.widht = 30
+        this.height = 30
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(
+            this.position.x, 
+            this.position.y, 
+            this.widht,
+             this.height
+        )
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) 
+        {
+            this.velocity.y += gravity
+        } else {
+            this.velocity.y = 0;
+        }
+        
+    }
+}
+
+class Platform {
+    constructor({x, y}) {
+        this.position = {
+            x,
+            y
+        }
+        this.widht = 200
+        this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(
+            this.position.x, 
+            this.position.y, 
+            this.widht, 
+            this.height
+        )
+    }
+}
+
+const player = new Player()
+const plataforms = [new Platform({x: 200, y: 100}), new Platform({x: 500, y: 200})]
+let scrollOffset = 0;
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+
+    plataforms.forEach(platform => {
+        platform.draw()
+    })
+
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5
+    } else {
+        player.velocity.x = 0
+
+        if (keys.right.pressed) {
+            scrollOffset += 5
+
+            plataforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+     
+        } else if (keys.left.pressed) {
+            scrollOffset -= 5
+
+            plataforms.forEach(platform => {
+                platform.position.x += 5
+            })
+        }
+    }
+    console.log(scrollOffset)
+
+    plataforms.forEach(platform => {
+    // Colisão plataforma
+    if (player.position.y + player.height <= platform.position.y && 
+        player.position.y + player.height + player.velocity.y >= platform.position.y && 
+        player.position.x + player.widht >= platform.position.x &&
+        player.position.x <= platform.position.x + platform.widht
+        ) 
+    {
+        player.velocity.y = 0
+    }
+    })
+
+    if (scrollOffset > 2000) {
+        console.log('you venceu')
+    }
+
+}
+
+animate()
+
+addEventListener('keydown', ({ keyCode }) => {
+
+    switch (keyCode) {
+        case 65:
+            keys.left.pressed = true
+            break
+        case 83:
+            player.velocity.y += 20;
+            break
+        case 68:
+            keys.right.pressed = true
+            break
+        case 87:
+            player.velocity.y -= 20;
+            break
+    }
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+
+    switch (keyCode) {
+        case 65:
+            keys.left.pressed = false
+            break
+        case 83:
+            player.velocity.y = 0;
+            break
+        case 68:
+            keys.right.pressed = false;
+            break
+        case 87:
+            player.velocity.y = 0;
+            break
+    }
+})
